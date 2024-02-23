6787e1cddc653906ad2ddab816392b117ab5fd65
Holger Kluck <holger.kluck@oeaw.ac.at>
2024-02-23 09:20:39 +0100

    Add skeleton of runAction

---
+++ include/runAction.hh
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 Holger Kluck
+ * This file runAction.hh is part of G4minWE.
+ *
+ * G4minWE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * G4minWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with G4minWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_RUNACTION_HH_
+#define INCLUDE_RUNACTION_HH_
+
+#include "G4UserRunAction.hh"
+class G4Run;
+
+class runAction : public G4UserRunAction{
+
+public:
+	runAction() = default;
+	~runAction() override = default;
+
+	void BeginOfRunAction(const G4Run*) override;
+	void   EndOfRunAction(const G4Run*) override;
+
+};
+
+#endif /* INCLUDE_RUNACTION_HH_ */
---
+++ src/runAction.cc
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2024 Holger Kluck
+ * This file runAction.cc is part of G4minWE.
+ *
+ * G4minWE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * G4minWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with G4minWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "runAction.hh"
+
+void runAction::BeginOfRunAction(const G4Run*) {
+}
+
+void runAction::EndOfRunAction(const G4Run*) {
+}
