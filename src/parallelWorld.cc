246f03c277efbfefc4f86e2d3a2cbf6f54fde53b
Holger Kluck <holger.kluck@oeaw.ac.at>
2024-06-03 20:07:46 +0200

    Add one row of replicated pixel

--- src/parallelWorld.cc
+++ src/parallelWorld.cc
@@ -20,12 +20,33 @@
 #include "G4VPhysicalVolume.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
+#include "G4PVReplica.hh"
+#include "G4Box.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4ThreeVector.hh"
 
 G4minWE::ParallelWorld::ParallelWorld(G4String name) : G4VUserParallelWorld(name){    
 }
 
 void G4minWE::ParallelWorld::Construct(){
     auto* world_logic = GetWorld()->GetLogicalVolume();
+
+    //Define one pixel: 50um x 50um, thickness 500um
+    G4double pix_x = 50.*um;
+    G4double pix_y = pix_x;
+    G4double pix_z = 500.*um;
+    auto* pixel_solid = new G4Box("pixel", pix_x/2., pix_y/2., pix_z/2);
+    auto* pixel_logic = new G4LogicalVolume(pixel_solid, nullptr, "pixel");
+
+    //Repeat pixel 160 times along x-axis
+    G4int rep_x = 160;
+    //Create an eveloping volume ... 
+    auto* row_solid = new G4Box("row", (rep_x*pix_x)/2., pix_y/2., pix_z/2.);
+    auto* row_logic = new G4LogicalVolume(row_solid, nullptr, "row");
+    new G4PVPlacement(nullptr, G4ThreeVector(-10.*cm, -10.*cm, -10.*cm), "row", row_logic, GetWorld(), false, 0);
+
+    // and fill it with pixels
+    new G4PVReplica("pix_repl", pixel_logic, row_logic, kXAxis, rep_x, pix_x);
 }
 
 void G4minWE::ParallelWorld::ConstructSD(){
